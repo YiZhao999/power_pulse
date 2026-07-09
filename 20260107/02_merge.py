7cb3ff469f654ae218beefdb06f90d71470b43e7
Yi Zhao <zoz20@pitt.edu>
2026-07-09 10:42:08 -0400

    Update session filter range in voting_df

--- 20260107/02_merge.py
+++ 20260107/02_merge.py
@@ -7,8 +7,8 @@ voting_df = pd.read_csv('voting_filtered.csv')
 # Filter aid_df for years 2000-2020
 aid_df = aid_df[(aid_df['year'] >= 2000) & (aid_df['year'] <= 2020)]
 
-# Filter voting_df for sessions 55-75
-voting_df = voting_df[(voting_df['session'] >= 55) & (voting_df['session'] <= 75)]
+# Filter voting_df for sessions 55-78 (previously 55-75)
+voting_df = voting_df[(voting_df['session'] >= 55) & (voting_df['session'] <= 78)]
 
 # Add year column to voting_df
 voting_df['year'] = voting_df['session'] + 1945
